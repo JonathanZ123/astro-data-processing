f476ed774ab074d6f5afaffde6798f29758a1060
Jonathan Zielinski <jonzb2022@gmail.com>
2026-07-25 12:59:25 -0400

    Update astro_data_reduction_notes.py

--- astro_data_reduction_notes.py
+++ astro_data_reduction_notes.py
@@ -20,10 +20,10 @@ from astropy.utils.exceptions import AstropyWarning
 warnings.simplefilter('ignore', category=AstropyWarning)
 
 # Fetch list of file paths for calibration and science frames
-dark_files = glob.glob(r"D:\SARA Data\112725RM\3c273\Dark30s_Empty_*.fits")
-bias_files = glob.glob(r"D:\SARA Data\112725RM\3c273\Bias_Empty_*.fits")
-flat_files = glob.glob(r"D:\SARA Data\112725RM\3c273\sarm20251111_flat_JohnsonV_*.fits")[:3]
-raw_science_files = glob.glob(r"D:\SARA Data\112725RM\3c273\3c273_Johnson_V_*_light.fits")[:3]
+dark_files = glob.glob(r"D:\your\file\here_*.fits")
+bias_files = glob.glob(r"D:\your\file\here_*.fits")
+flat_files = glob.glob(r"D:\your\file\here_*.fits")
+raw_science_files = glob.glob(r"D:\your\file\here_*.fits")
 
 # Output counts of located calibration and target frames
 print("Dark Files Found:", len(dark_files))
